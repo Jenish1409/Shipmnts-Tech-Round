aeed8fbae5b1867358ee09b878a2df2f1b3bf05d
unknown <jenishraichura58@gmail.com>
2026-08-06 14:00:58 +0530

    Added GET /users for list of all the users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -16,4 +16,16 @@ router.post('/', userValidationRules, async (req, res) => {
     }
 });
 
+router.get('/', async(req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({users});
+    } catch (error) {
+        res.status(500).json({
+            error: error.code,
+            message: error.message
+        });
+    }
+});
+
 module.exports = router;
